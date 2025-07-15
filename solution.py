7fd56cf0a1c057774d7fefa54e13cc9d8426217a
Aliff Razak <59641814+aliffrazak02@users.noreply.github.com>
2025-07-15 15:55:08 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -72,3 +72,17 @@ class Solution:
             numMap[nums[i]] = i
             
         return []
+
+# 1365. How Many Numbers Are Smaller Than the Current Number
+# Given the array nums, for each nums[i], find out how many numbers in the array are smaller than it. 
+# That is, for each nums[i], you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+# Return the answer in an array.
+
+# Brute force solution
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        arr = [0]*len(nums)
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] != nums[j] and nums[i]>nums[j]:
+                    arr[i] += 1
+        return arr
