05ab490a88229cd57bc5be10f5bb367fae16d3e7
Aliff Razak <59641814+aliffrazak02@users.noreply.github.com>
2025-07-24 13:54:09 +0800

    Update solution.py Q.125

--- solution.py
+++ solution.py
@@ -194,6 +194,28 @@ class Solution:
 
         return sMap == tMap
 
+# 125. Valid Palindrome
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and 
+# removing all non-alphanumeric characters, it reads the same forward and backward. 
+# Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Static sliding window solution, O(n)
+    def isPalindrome(self, s: str) -> bool:
+        l,r = 0,len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+
+
 
 
 
